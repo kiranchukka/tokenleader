0372332071b7b16636acc668f2cf651bd1d2b3ff
Kiran Kumar <kiran.c@itc.in>
2019-03-01 19:13:26 +0530

    v

--- tokenleader/app_run.py
+++ tokenleader/app_run.py
@@ -6,7 +6,7 @@ from tokenleader.app1.configs import configs
 
 from tokenleader.app1.authentication.token_after_login import token_login_bp
 from tokenleader.app1.adminops.adminops_restapi import adminops_bp
-from app1.catalog.catalog_functions import catalog_bp
+from tokenleader.app1.catalog.catalog_functions import catalog_bp
 from tokenleader.app1.catalog import models_catalog 
 
 #bp_list = [token_login_bp, catalog_bp]
