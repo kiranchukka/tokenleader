c1da928de57b23d4206b75978952283595340792
Kiran Kumar <kiran.c@itc.in>
2019-03-01 19:05:42 +0530

    a

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -1,21 +1,22 @@
 from flask import request, Blueprint, jsonify, current_app,make_response
-from tokenleader.app1.catalog import catalog_functions as cf
+from tokenleader.app1.catalog import catalog_functions as cf 
 from tokenleaderclient.configs.config_handler import Configs    
 from tokenleaderclient.client.client import Client 
 from tokenleaderclient.rbac.enforcer import Enforcer
+from tokenleader.app1.adminops.adminops_restapi import adminops_bp
 
-adminops_bp = Blueprint('adminops_bp', __name__)
+
 auth_config = Configs()
 tlclient = Client(auth_config)
 enforcer = Enforcer(tlclient)
  
    
-@adminops_bp.route('/list/addservice/<servicename>/<pwd>/<urlint>/<urlext>/<urladmin>', methods=['GET'])
-@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_users')
-def add_service(wfc,servicename,pwd,urlint,urlext,urladmin):    
+#@catalog_bp.route('/list/addservice/<servicename>/<pwd>/<urlint>/<urlext>/<urladmin>', methods=['GET'])
+#@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_users')
+#def add_service(wfc,servicename,pwd,urlint,urlext,urladmin):    
     
-    msg = cf.add_service(servicename, pwd, urlint, urlext, urladmin )     
-    return msg
+#    msg = cf.add_service(servicename, pwd, urlint, urlext, urladmin )     
+#    return msg
 
 @adminops_bp.route('/list/services', methods=['GET'])
 def list_services():    
